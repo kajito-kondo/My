5a053cee9dd71051a44eeb16870f0cb914ddcb57
kajito-kondo <241205082@ccmailg.meijo-u.ac.jp>
2025-09-04 14:12:25 +0900

    画像へのアクセス

--- app/src/main/java/com/example/myapp_test/MainActivity6.java
+++ app/src/main/java/com/example/myapp_test/MainActivity6.java
@@ -1,6 +1,7 @@
 package com.example.myapp_test;
 
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -35,23 +36,28 @@ public class MainActivity6 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // リクエスト先に画像URLを指定
+        // getImage("https://placehold.jp/350x350.png");
+        binding.button25.setOnClickListener(v -> {
+            var text = binding.editTextText3.getText().toString();
+            var url = Uri.parse("https://placehold.jp/350x350.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+            getImage(url);
+        });
+        }
+    private void getImage(String url){
         var request = new Request.Builder()
-                .url("https://placehold.jp/350x350.png")
+                .url(url)
                 .build();
-
-        // 非同期通信でリクエスト
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                // 通信に失敗した時に呼ばれる
             }
-
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                // InputStreamをBitmapに変換
                 var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
-                // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
                 runOnUiThread(() -> binding.imageView2.setImageBitmap(bitmap));
             }
         });
