bd4a77deb0bb23fbf9878e4bc8dddbdb4e7dc534
kajito-kondo <241205082@ccmailg.meijo-u.ac.jp>
2025-09-04 13:28:06 +0900

    画像へのアクセス

---
+++ app/src/main/java/com/example/myapp_test/MainActivity6.java
@@ -0,0 +1,59 @@
+package com.example.myapp_test;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapp_test.databinding.ActivityMain6Binding;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity6 extends AppCompatActivity {
+    private ActivityMain6Binding binding;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain6Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // リクエスト先に画像URLを指定
+        var request = new Request.Builder()
+                .url("https://placehold.jp/350x350.png")
+                .build();
+
+        // 非同期通信でリクエスト
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                // 通信に失敗した時に呼ばれる
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // InputStreamをBitmapに変換
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
+                runOnUiThread(() -> binding.imageView2.setImageBitmap(bitmap));
+            }
+        });
+    }
+}
